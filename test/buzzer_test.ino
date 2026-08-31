6c17c445934a57ae5b4eaba125e216466ea516ff
Md Yasin Mondal <yasheditz87@gmail.com>
2026-08-31 17:50:34 +0530

    Implement buzzer test with serial output
    Added a test for the buzzer functionality with serial output.

--- test/buzzer_test.ino
+++ test/buzzer_test.ino
@@ -1,1 +1,31 @@
 
+#define BUZZER D7
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(BUZZER, OUTPUT);
+
+  digitalWrite(BUZZER, LOW);
+
+  Serial.println();
+  Serial.println("==============================");
+  Serial.println("        BUZZER TEST");
+  Serial.println("==============================");
+}
+
+void loop() {
+
+  Serial.println("BUZZER ON");
+
+  digitalWrite(BUZZER, HIGH);
+
+  delay(2000);
+
+  Serial.println("BUZZER OFF");
+
+  digitalWrite(BUZZER, LOW);
+
+  delay(2000);
+}
