10e8f1c2590365f605aa8e5995fb9e22680be787
Md Yasin Mondal <yasheditz87@gmail.com>
2026-08-31 17:51:07 +0530

    Implement button test with serial feedback
    Added button test functionality with serial output.

--- test/button_test.ino
+++ test/button_test.ino
@@ -1,1 +1,31 @@
 
+#define BUTTON D3
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  Serial.println();
+  Serial.println("==============================");
+  Serial.println("       BUTTON TEST");
+  Serial.println("==============================");
+  Serial.println("Press the button...");
+}
+
+void loop() {
+
+  if (digitalRead(BUTTON) == LOW) {
+
+    Serial.println("BUTTON PRESSED");
+
+    delay(300);
+
+  } else {
+
+    Serial.println("BUTTON RELEASED");
+
+    delay(300);
+  }
+}
